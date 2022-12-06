6fcf950dcb4f2ba0b4e409b47d40c91cc2f6c21a
emsleyd <emsleyd@gmail.com>
2022-12-06 15:58:33 +0000

    passed books as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,80 @@
-import * as React from 'react';
+import * as React from "react";
 
 const title = {
-  name: 'Book Book Book',
-  tagline: 'Personal Book Library',
+  name: "Book Book Book",
+  tagline: "Personal Book Library",
 };
 
-const App = () => (
-  <div>
+const App = () => {
+  const books = [
+    {
+      title: "Project Hail Mary",
+      author: "Andy Weir",
+      url: "https://en.wikipedia.org/wiki/Project_Hail_Mary",
+      num_comments: 2,
+      rating: 4.5,
+      objectID: 0,
+    },
+    {
+      title: "The Martian",
+      author: "Andy Weir",
+      url: "https://en.wikipedia.org/wiki/The_Martian_(Weir_novel)",
+      num_comments: 4,
+      rating: 5,
+      objectID: 1,
+    },
+    {
+      title: "The Long Earth",
+      author: "Terry Pratchett",
+      url: "https://en.wikipedia.org/wiki/The_Long_Earth",
+      num_comments: 1,
+      rating: 4,
+      objectID: 2,
+    },
+  ];
 
-    <h1>{title.name}</h1>
-    <h3>{title.tagline}</h3>
+  const Search = () => (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
+    </div>
+  );
 
-    <Search />
-
-    <br />
-    <hr />
-
-    <List />
-
-  </div>
-);
-
-const Search = () => (
-  <div>
-    <label htmlFor='search'>Search: </label>
-    <input id="search" type="text"></input>
-  </div>
-);
-
-const List = () => (
-  <div>
+  const List = (props) => (
     <ul>
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title} </a>
-          </span>
-          <span>by {item.author}</span>
-          <br />
-          <span>Number of comments: {item.num_comments}</span>
-          <br />
-          <span>Rating: {item.rating}</span>
-        </li>
-      ))}
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+          ))}
     </ul>
-  </div>
-);
+      
+); 
+          
+    const Item = (props) => (
+      <li>
+      <span>
+        <a href={props.item.url}>{props.item.title} </a>
+      </span>
+      <span>by {props.item.author}</span>
+      <br />
+      <span>Number of comments: {props.item.num_comments}</span>
+      <br />
+      <span>Rating: {props.item.rating}</span>
+      </li>
+      );
 
+      return (
+      <div>
+        <h1>{title.name}</h1>
+        <h3>{title.tagline}</h3>
 
-const list = [
-  {
-    title: 'Project Hail Mary',
-    author: 'Andy Weir',
-    url: 'https://en.wikipedia.org/wiki/Project_Hail_Mary',
-    num_comments: 2,
-    rating: 4.5,
-    objectID: 0,
-  },
-  {
-    title: 'The Martian',
-    author: 'Andy Weir',
-    url: 'https://en.wikipedia.org/wiki/The_Martian_(Weir_novel)',
-    num_comments: 4,
-    rating: 5,
-    objectID: 1,
-  },
-  {
-    title: 'The Long Earth',
-    author: 'Terry Pratchett',
-    url: 'https://en.wikipedia.org/wiki/The_Long_Earth',
-    num_comments: 1,
-    rating: 4,
-    objectID: 2,
-  }
-]
+        <Search />
 
-export default App; 
+        <br />
+        <hr />
+
+        <List list={books} />
+      </div>
+      );
+};
+
+      export default App;
