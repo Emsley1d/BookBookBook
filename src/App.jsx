595a0037870aa6484a5732b5aab7fc6c031b8785
emsleyd <emsleyd@gmail.com>
2022-12-06 14:17:22 +0000

    refactored correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,78 @@
 import * as React from 'react';
-import List from './List';
-
-
 
 const title = {
   name: 'Book Book Book',
   tagline: 'Personal Book Library',
 };
 
-const App = () => {
-    <div>
+const App = () => (
+  <div>
 
-      <h1>{title.name}</h1>
-      <h3>{title.tagline}</h3>
+    <h1>{title.name}</h1>
+    <h3>{title.tagline}</h3>
 
-      <Search />
+    <Search />
+
+    <br />
+    <hr />
+
+    <List />
+
+  </div>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor='search'>Search: </label>
+    <input id="search" type="text"></input>
+  </div>
+);
+
+const List = () => (
+  <div>
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title} </a>
+          </span>
+          <span>by {item.author}</span>
+          <br />
+          <span>Number of comments: {item.num_comments}</span>
+          <br />
+          <span>Rating: {item.rating}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 
-      <br />
-      <hr />
-
-      <List />
-
-    </div>
-};
-
-const Search = () => {
-    <div>
-      <label htmlFor='search'>Search: </label>
-      <input id="search" type="text"></input>
-    </div>
-};
-
+const list = [
+  {
+    title: 'Project Hail Mary',
+    author: 'Andy Weir',
+    url: 'https://en.wikipedia.org/wiki/Project_Hail_Mary',
+    num_comments: 2,
+    rating: 4.5,
+    objectID: 0,
+  },
+  {
+    title: 'The Martian',
+    author: 'Andy Weir',
+    url: 'https://en.wikipedia.org/wiki/The_Martian_(Weir_novel)',
+    num_comments: 4,
+    rating: 5,
+    objectID: 1,
+  },
+  {
+    title: 'The Long Earth',
+    author: 'Terry Pratchett',
+    url: 'https://en.wikipedia.org/wiki/The_Long_Earth',
+    num_comments: 1,
+    rating: 4,
+    objectID: 2,
+  }
+]
 
 export default App; 
