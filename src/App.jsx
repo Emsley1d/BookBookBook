0f75bacf2842b8e78917fff93d4abeac606fbe7e
emsleyd <emsleyd@gmail.com>
2022-12-07 16:19:12 +0000

    local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,16 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+    setSearchTerm(event.target.value)
   };
 
   const searchedBooks = books.filter((book) =>
