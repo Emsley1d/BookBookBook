6fbbef8700d57480fb02ef6c2eddb2027586ee52
emsleyd <emsleyd@gmail.com>
2022-12-08 12:36:56 +0000

    remove function added

--- src/App.jsx
+++ src/App.jsx
@@ -17,35 +17,46 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const initialBooks = [
+  {
+    title: "Project Hail Mary",
+    author: "Andy Weir",
+    url: "https://en.wikipedia.org/wiki/Project_Hail_Mary",
+    num_comments: 2,
+    rating: 4.5,
+    objectID: 0,
+  },
+  {
+    title: "The Martian",
+    author: "Andy Weir",
+    url: "https://en.wikipedia.org/wiki/The_Martian_(Weir_novel)",
+    num_comments: 4,
+    rating: 5,
+    objectID: 1,
+  },
+  {
+    title: "The Long Earth",
+    author: "Terry Pratchett",
+    url: "https://en.wikipedia.org/wiki/The_Long_Earth",
+    num_comments: 1,
+    rating: 4,
+    objectID: 2,
+  },
+];
+
 const App = () => {
-  const books = [
-    {
-      title: "Project Hail Mary",
-      author: "Andy Weir",
-      url: "https://en.wikipedia.org/wiki/Project_Hail_Mary",
-      num_comments: 2,
-      rating: 4.5,
-      objectID: 0,
-    },
-    {
-      title: "The Martian",
-      author: "Andy Weir",
-      url: "https://en.wikipedia.org/wiki/The_Martian_(Weir_novel)",
-      num_comments: 4,
-      rating: 5,
-      objectID: 1,
-    },
-    {
-      title: "The Long Earth",
-      author: "Terry Pratchett",
-      url: "https://en.wikipedia.org/wiki/The_Long_Earth",
-      num_comments: 1,
-      rating: 4,
-      objectID: 2,
-    },
-  ];
 
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+
+  const [books, setBooks] = React.useState(initialBooks);
+
+  const handleRemoveBook = (item) => {
+    const newBooks = books.filter(
+      (book) => item.objectID !== book.objectID
+    );
+
+    setBooks(newBooks)
+  };
 
   React.useEffect(() => {
     localStorage.setItem("search", searchTerm);
@@ -68,6 +79,7 @@ const App = () => {
         id="search"
         label="Search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
 
@@ -78,41 +90,75 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedBooks} />
+      <List list={searchedBooks} onRemoveItem={handleRemoveBook} />
     </div>
   );
 };
 
-const InputWithLabel = ({ id, label, value, type = "text", onInputChange, children }) => (
-  <>
-    {" "}
-    {/* this is a react fragment; shorthand version of <React.Fragment> */}
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange} />
-  </>
-);
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
 
-const List = ({ list }) => (
-  <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
+  const inputRef = React.useRef();
 
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <br />
-    <span>Author: {item.author}</span>
-    <br />
-    <span>Comments: {item.num_comments}</span>
-    <br />
-    <span>Rating: {item.rating}</span>
-  </li>
-);
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
 
-export default App;
+  return (
+    <>
+      {/* {" "} */}
+      {/* this is a react fragment; shorthand version of <React.Fragment> */}
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef} 
+        id={id} 
+        type={type} 
+        value={value} 
+        autoFocus={isFocused} 
+        onChange={onInputChange} />
+    </>
+  );
+  }
+
+  const List = ({ list, onRemoveItem }) => (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </ul>
+  );
+
+  const Item = ({ item, onRemoveItem }) => {
+    const handleRemoveItem = () => {
+      onRemoveItem(item);
+    };
+    return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <br />
+      <span>Author: {item.author}</span>
+      <br />
+      <span>Comments: {item.num_comments}</span>
+      <br />
+      <span>Rating: {item.rating}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Remove
+          </button>
+      </span>
+    </li>
+  );
+    };
+  export default App;
